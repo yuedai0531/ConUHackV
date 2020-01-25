7ffc6f318988183e75d8b6c82c24ea413f8b877e
Yue Dai <yue.dai@elementai.com>
2020-01-25 15:03:01 -0500

    GCP

--- emotions.py
+++ emotions.py
@@ -21,6 +21,52 @@ image = types.Image(content=content)
 response = client.label_detection(image=image)
 labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+# print('Labels:')
+# for label in labels:
+#     print(label.description)
+
+
+
+def detect_faces(path):
+    """Detects faces in an image."""
+
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    count = 0
+    for face in faces:
+        print(count)
+        count += 1
+
+        print(face)
+
+        # print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        # print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        # print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+detect_faces('ohwonder.jpg')
+
